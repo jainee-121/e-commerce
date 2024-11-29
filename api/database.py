97250739f17510d8d328e1f63bbd083d00c594e4
jainee-121 <jainee1215.ahm@gmail.com>
2024-11-29 18:08:59 +0530

    applied crud

--- api/database.py
+++ api/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from . import DB
 
 from dotenv import load_dotenv
 
 load_dotenv()
-import os
-DB_URL = DB_URL = os.getenv("DB_URL")
-engine = create_engine(DB_URL,echo=True)
+engine = create_engine(DB.DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
 Base = declarative_base()
